a7f184de8d5fa44d671a27553311692fb1694d25
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-03-20 13:38:46 +0000

    added some end implemented the end points

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\auth;
 use App\Http\Controllers\plant;
+use App\Http\Controllers\Catigoie;
 
 Route::put("auth/singUp",[auth::class,"signUP"]);
 Route::put("auth/singUp",[auth::class,"login"]);
@@ -11,4 +12,8 @@ Route::middleware(["JWT_validation","isAdmin"])->group(function(){
     Route::post("plant/add",[plant::class,"addPlant"]);
     Route::patch("plant/update/{id}",[plant::class,"updatePlant"]);
     Route::delete("plant/delete/{id}",[plant::class,"deletePlant"]);
+    
+    Route::post("category/add", [Catigoie::class, "addCategory"]);
+    Route::patch("category/update/{id}", [Catigoie::class, "updateCategory"]);
+    Route::delete("category/delete/{id}", [Catigoie::class, "deleteCategory"]);
 });
