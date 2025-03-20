08005bdececc68c2eea02fefd74bd6b23e9bcd14
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-03-20 12:35:39 +0000

    implemented the update plant method

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::put("auth/singUp",[auth::class,"signUP"]);
 Route::put("auth/singUp",[auth::class,"login"]);
 Route::middleware(["JWT_validation","isAdmin"])->group(function(){
     Route::post("plant/add",[plant::class,"addPlant"]);
+    Route::post("plant/update/{id}",[plant::class,"updatePlant"]);
 });
