5d29592f66a09b377ef7572af4eed7b5e7e3b97e
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-03-25 13:21:37 -0700

    implemented the fucntion's

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\auth;
 use App\Http\Controllers\plant;
 use App\Http\Controllers\Catigoie;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\saleController;
 
 Route::put("auth/singUp",[auth::class,"signUP"]);
 Route::put("auth/singUp",[auth::class,"login"]);
@@ -18,6 +19,10 @@ Route::middleware(["JWT_validation","isAdmin"])->group(function(){
     Route::post("category/add", [Catigoie::class, "addCategory"]);
     Route::patch("category/update/{id}", [Catigoie::class, "updateCategory"]);
     Route::delete("category/delete/{id}", [Catigoie::class, "deleteCategory"]);
+
+    Route::get("statistec/totalesSales",[saleController::class,"totaleTales"]);
+    Route::get("statistec/popularPlants",[saleController::class,"popularPlants"]);
+    Route::get("statistec/salesByCatigory",[saleController::class,"salesByCatigory"]);
 });
 Route::middleware(["JWT_validation","isClient"])->group(function(){
     Route::post("order/create", [OrderController::class, "createOrder"]);
