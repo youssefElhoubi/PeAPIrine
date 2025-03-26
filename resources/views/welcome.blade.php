f5dc301195962a868792629c6c0407f48b71e21b
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-03-26 14:09:01 +0000

    "Added font link to welcome.blade.php"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
